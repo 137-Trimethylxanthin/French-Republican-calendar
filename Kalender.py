7027c5bac7b4b8d94abf51844a68aee30db78925
Methyltheobromin <95408324+137-Trimethylxanthin@users.noreply.github.com>
2022-08-23 14:37:50 +0200

    Update Kalender.py

--- Kalender.py
+++ Kalender.py
@@ -1,8 +1,19 @@
-import datetime
-from time import sleep
 import PySimpleGUI as sg
+from time import sleep
+import datetime
+import sys
+import subprocess
+import pkg_resources
+
+required = {'pysimplegui'}
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = required - installed
 
 
+if missing:
+    python = sys.executable
+    subprocess.check_call(
+        [python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
 
 
 def romanNummeral(num):
@@ -31,12 +42,13 @@ def romanNummeral(num):
 
 
 def revulutionsDays():
-    day = ['Primidi', 'Duodi', 'Tridi', 'Quartidi', 'Quintidi','Sextidi', 'Septidi', 'Octidi', 'Nonidi', 'Decadi']
+    day = ['Primidi', 'Duodi', 'Tridi', 'Quartidi', 'Quintidi',
+           'Sextidi', 'Septidi', 'Octidi', 'Nonidi', 'Decadi']
     today = datetime.date.today()
     creationDay = datetime.date(1789, 7, 15)
     delta = today - creationDay
     foo = str(delta.days)[-1]
-    
+
     return day[int(foo)]
 
 
@@ -50,20 +62,21 @@ def revulutionsYears():
 
 def revulutionsTime():
     getTime = datetime.datetime.now().time()
-    timeDeRevulution = int((getTime.hour * 3600 + getTime.minute * 60 + getTime.second)//0.864)        
+    timeDeRevulution = int(
+        (getTime.hour * 3600 + getTime.minute * 60 + getTime.second)//0.864)
     hour = str(timeDeRevulution).zfill(5)[0]
     minute1 = str(timeDeRevulution).zfill(5)[1]
     minute2 = str(timeDeRevulution).zfill(5)[2]
     sec1 = str(timeDeRevulution).zfill(5)[3]
     sec2 = str(timeDeRevulution).zfill(5)[4]
-    return hour , minute1, minute2, sec1, sec2
+    return hour, minute1, minute2, sec1, sec2
 
 
 def revulutionsMonths():
     year = datetime.date.today().year
     today = datetime.date.today()
     month = ['Vendémiaire', 'Brumaire', 'Frimaire', 'Nivôse', 'Pluviôse', 'Ventôse',
-         'Germinal', 'Floréal', 'Prairial', 'Messidor', 'Thermidor', 'Fructidor']
+             'Germinal', 'Floréal', 'Prairial', 'Messidor', 'Thermidor', 'Fructidor']
 
     start = datetime.date(year, 9, 22)
     end = datetime.date(year, 10, 21)
@@ -131,9 +144,7 @@ def revulutionsMonths():
 #
 
 
-
-
-#ask what do do:
+# ask what do do:
 
 def asking():
     print(' ')
@@ -144,11 +155,8 @@ def asking():
     print(f'you selected {ask}')
     sleep(1)
 
-
-
-
-    #print terminal:
-    if ask.lower() == 'cli'or ask.lower()=='c':
+    # print terminal:
+    if ask.lower() == 'cli' or ask.lower() == 'c':
         hour, minute1, minute2, sec1, sec2 = revulutionsTime()
         days = revulutionsDays()
         months = revulutionsMonths()
@@ -156,39 +164,43 @@ def asking():
         years = revulutionsYears()
         print(' ')
         print(' ')
-        print(f'the day is: {days}  this month is: {months}    {roman}|{years}  years of freedom', sep='')
+        print(
+            f'the day is: {days}  this month is: {months}    {roman}|{years}  years of freedom', sep='')
         print(f'{hour}:{minute1}{minute2}:{sec1}{sec2}')
         print(' ')
         print(' ')
         exit()
 
-    #GUI here:
-    if ask.lower() == 'gui'or ask.lower()=='g':
+    # GUI here:
+    if ask.lower() == 'gui' or ask.lower() == 'g':
 
-        sg.theme('DarkAmber') 
+        sg.theme('DarkAmber')
 
-        layout = [  [sg.Text('Revolution', expand_y=True)],
-                    [sg.Text('the day is: ' ), sg.Text('', key='_day_')],
-                    [sg.Text('this month is: '), sg.Text('', key='_month_')],
-                    [sg.Text('years of freedom: '), sg.Text('', key='_year_')],
-                    [sg.Text('Time: ', expand_y=True), sg.Text('', key='_time_', expand_y=True)],
-                    [(sg.Quit())]
-                    ]
-
-
+        layout = [[sg.Text('Revolution', expand_y=True)],
+                  [sg.Text('the day is: '), sg.Text('', key='_day_')],
+                  [sg.Text('this month is: '), sg.Text('', key='_month_')],
+                  [sg.Text('years of freedom: '), sg.Text('', key='_year_')],
+                  [sg.Text('Time: ', expand_y=True), sg.Text(
+                      '', key='_time_', expand_y=True)],
+                  [(sg.Quit())]
+                  ]
 
         # Create the window object
-        window = sg.Window('Revolution',size=(500,300), element_justification='center').Layout(layout)
+        window = sg.Window('Revolution', size=(500, 300),
+                           element_justification='center').Layout(layout)
 
         def getTime():
             hour, minute1, minute2, sec1, sec2 = revulutionsTime()
             return f'{hour}:{minute1}{minute2}:{sec1}{sec2}'
+
         def getDay():
             days = revulutionsDays()
             return f'{days}'
+
         def getMonth():
             months = revulutionsMonths()
             return f'{months}'
+
         def getYear():
             roman = romanNummeral(revulutionsYears())
             years = revulutionsYears()
@@ -215,5 +227,6 @@ def asking():
         print('error wrong input pease try again')
         sleep(1)
         asking()
-        
+
+
 asking()
